588a222746200348d1bdf75449fd00a9d9b2a4bd
lydialydia-lydia <yuxyawang@gmail.com>
2026-01-19 17:25:55 -0500

    Update paths.py

--- src/b2risk/paths.py
+++ src/b2risk/paths.py
@@ -1,8 +1,6 @@
 import os
 
 def get_project_root() -> str:
-    # Assumes notebooks/ is one level below repo root
-    # If running from repo root, this still works if you set root manually.
     return os.path.abspath(os.path.join(os.getcwd(), "..")) if os.path.basename(os.getcwd()) == "notebooks" else os.getcwd()
 
 def get_data_dirs(root: str):
